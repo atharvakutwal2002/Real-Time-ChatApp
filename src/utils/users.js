9c022106ffff19a37930392be44009a752453ebd
Atharva <atharvakutwal2002@gmail.com>
2022-08-09 15:46:27 +0530

    users.js configured

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,33 +1,38 @@
 const users = [];
 
 const addUser = ({ id, username, room }) => {
+  // Clean the data
   username = username.trim().toLowerCase();
   room = room.trim().toLowerCase();
 
+  // Validate the data
   if (!username || !room) {
     return {
-      error: " Username and room are required ",
+      error: "Username and room are required!",
     };
   }
+
+  // Check for existing user
   const existingUser = users.find((user) => {
-    return users.room === room && user.username === username;
+    return user.room === room && user.username === username;
   });
 
+  // Validate username
   if (existingUser) {
     return {
-      error: " Username already exists ! ",
+      error: "Username is in use!",
     };
   }
 
+  // Store user
   const user = { id, username, room };
   users.push(user);
   return { user };
 };
 
 const removeUser = (id) => {
-  const index = users.findIndex((user) => {
-    return user.id === id;
-  });
+  const index = users.findIndex((user) => user.id === id);
+
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
@@ -42,9 +47,9 @@ const getUsersInRoom = (room) => {
   return users.filter((user) => user.room === room);
 };
 
-module.exports={
-    addUser,
-    removeUser,
-    getUser,
-    getUsersInRoom
-}
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
